ef8dbdebb322399848029b8defac64d61dbc2682
Taylor Hogge <thogge@swellcx.com>
2024-08-14 12:05:57 -0600

    Set up knex

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import Knex from "knex";
+
 dotenv.config();
 
+const knex = Knex({
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+});
+
 const client = new OpenAI(process.env.OPENAI_API_KEY);
+
+const main = async () => {
+    const responses = await knex('responses').select('*');
+    console.log(responses);
+};
+
+const cleanup = async () => {
+    await knex.destroy();
+};
+
+main().then(cleanup);
