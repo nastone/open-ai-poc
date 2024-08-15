94e6efc1e53ac3a4c6fc0536884409722d8cdc00
Taylor Hogge <thogge@swellcx.com>
2024-08-15 10:05:12 -0600

    Improve matchThemesWithSentiment by preventing it from producing multiple instances of the same theme (with different sentiments)

--- src/functions/match-themes-with-sentiment.ts
+++ src/functions/match-themes-with-sentiment.ts
@@ -1,21 +1,35 @@
 import { getEmbedding, knex } from "../connector.js";
 
 export const matchThemesWithSentiment = async (text: string) => {
-  if (!text) throw new Error("Must provide input text");
-  const embedding = await getEmbedding(text);
-  const results = await knex
-    .select({
-      category: "categories.name",
-      sentiment: "sentiments.name",
-      distance: knex.raw(
-        "(categories.embedding + sentiments.embedding) <=> ?",
-        [embedding]
-      ),
-    })
-    .from("categories")
-    .crossJoin(knex.raw("sentiments"))
-    .orderBy("distance", "asc")
-    .limit(5);
-
-  console.table(results);
+  if (!text) throw new Error('Must provide input text');
+    const embedding = await getEmbedding(text);
+    const results = await knex.with('distances',
+        knex.select({
+            category: 'categories.name',
+            sentiment: 'sentiments.name',
+            distance: knex.raw('(categories.embedding + sentiments.embedding) <=> ?', [embedding])
+        })
+        .from('categories')
+        .crossJoin(knex.raw('sentiments'))
+        .orderBy('distance', 'asc')
+    )
+     .with('min_distances',
+        knex.select({
+            category: 'category',
+            distance: knex.raw('MIN(distance)'),
+        })
+        .from('distances')
+        .groupBy('category')
+     )
+     .select({
+        category: 'min_distances.category',
+        sentiment: 'distances.sentiment',
+        distance: 'min_distances.distance',
+     })
+     .from('min_distances')
+     .join('distances', 'min_distances.distance', 'distances.distance')
+     .orderBy('distance', 'asc')
+     .limit(5);
+        
+    console.table(results);
 };
